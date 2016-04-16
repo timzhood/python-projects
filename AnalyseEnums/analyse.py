2e31c7ef41f9e0b4b85f89e9ec0af88758484a3d
tim <tim.z.hood@gmail.com>
2016-04-16 22:36:53 +0100

    Print CSV containing all the enums fully expanded.

--- AnalyseEnums/analyse.py
+++ AnalyseEnums/analyse.py
@@ -2,6 +2,7 @@
 """
 Analyse a set of enumerations.
 """
+import csv
 
 
 def main(file_name):
@@ -35,6 +36,36 @@ def main(file_name):
     total_list = list(total_set)
     total_list.sort()
     print(total_list)
+
+    # expand each of the enum lists
+    expanded_enums = dict()
+
+    for enum_name in enums.keys():
+        expanded_enums[enum_name] = list()
+        for value in enums[enum_name]:
+            index = total_list.index(value)
+            while index >= len(expanded_enums[enum_name]):
+                expanded_enums[enum_name].append('')
+            expanded_enums[enum_name][index] = value
+        print(expanded_enums[enum_name])
+
+    for enum_name in expanded_enums.keys():
+        while len(expanded_enums[enum_name]) < len(total_list):
+            expanded_enums[enum_name].append('')
+
+    with open('some.csv', 'wb') as f:
+        writer = csv.writer(f)
+        header_row = list()
+        header_row.append('all')
+        for enum_name in sorted(expanded_enums.keys()):
+            header_row.append(enum_name)
+        writer.writerow(header_row)
+        for index in range(len(total_list)):
+            row = list()
+            row.append(total_list[index])
+            for enum_name in sorted(expanded_enums.keys()):
+                row.append(expanded_enums[enum_name][index])
+            writer.writerow(row)
 
 
 if __name__ == '__main__':
