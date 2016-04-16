38c1fa8c2959ec4159e72d033a345df6215c9d2c
tim <tim.z.hood@gmail.com>
2016-04-16 19:38:10 +0000

    Copy from PycharmProjects.

---
+++ AnalyseEnums/analyse.py
@@ -0,0 +1,41 @@
+# coding=utf-8
+"""
+Analyse a set of enumerations.
+"""
+
+
+def main(file_name):
+    """
+    Read in enums from file.
+    :param file_name: the file
+    """
+
+    # make a dictionary of lists by enum name
+    enums = dict()
+
+    type_prefix = 'TYPE='
+
+    with open(file_name) as in_file:
+        lines = in_file.readlines()
+        enum_name = None
+        for line in lines:
+            if line.startswith(type_prefix):
+                enum_name = line.split(type_prefix, 1)[1].strip()
+                enums[enum_name] = list()
+            else:
+                enums[enum_name].append(line.strip())
+
+    # make complete master list
+    total_set = set()
+    for enum in enums.values():
+        total_set.update(enum)
+    print(total_set)
+
+    # make a sorted list
+    total_list = list(total_set)
+    total_list.sort()
+    print(total_list)
+
+
+if __name__ == '__main__':
+    main('jim.txt')
